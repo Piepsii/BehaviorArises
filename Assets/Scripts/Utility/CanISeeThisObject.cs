269203c01f04c78e9286ee226feac9d52e39a57d
Paul Brandstetter <paulbrandstetter@tuta.io>
2022-06-28 13:55:39 +0200

    Utility: CanISeeThisObject
    CanISeeThisObject checks if it's gameObject can be seen by the main camera and logs the result in the console.

---
+++ Assets/Scripts/Utility/CanISeeThisObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace BehaviorArises.Utility
+{
+    public class CanISeeThisObject : MonoBehaviour
+    {
+        GameObject obj;
+        Collider objCollider;
+
+        Camera cam;
+        Plane[] planes;
+
+        void Start()
+        {
+            obj = gameObject;
+            cam = Camera.main;
+            objCollider = GetComponent<Collider>();
+        }
+
+        void Update()
+        {
+            planes = GeometryUtility.CalculateFrustumPlanes(cam);
+            if (GeometryUtility.TestPlanesAABB(planes, objCollider.bounds))
+            {
+                Debug.Log(obj.name + " has been detected!");
+            }
+            else
+            {
+                Debug.Log("Nothing has been detected.");
+            }
+        }
+
+    }
+}
